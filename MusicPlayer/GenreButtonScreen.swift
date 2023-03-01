b940cd0d7217a3b24b1315be401ba3074627bdb9
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-03-01 16:37:40 -0300

    class GenreButtonScreen created

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,29 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Jonathan Marcelo Peres on 01/03/23.
+//
+
+import UIKit
+
+class GenreButtonScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
