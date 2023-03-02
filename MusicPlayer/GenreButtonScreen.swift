33a35e880410a90b93ee2c90ccf2c051bf9dd1e1
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-03-02 11:46:34 -0300

    func formatButtons set

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -10,11 +10,13 @@ import MediaPlayer
 
 class GenreButtonScreen: UIViewController {
     
+    @IBOutlet var musicPlayerButtons: [UIButton]!
+        
     var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        formatButtons()
     }
 
     @IBAction func genreButtonTapped(_ sender: UIButton) {
@@ -37,14 +39,20 @@ class GenreButtonScreen: UIViewController {
     func playGenre(genre: String) {
         
         musicPlayer.stop()
-        
+
         let query = MPMediaQuery()
         let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
-        
+
         query.addFilterPredicate(predicate)
-        
+
         musicPlayer.setQueue(with: query)
         musicPlayer.shuffleMode = .songs
         musicPlayer.play()
+    }
+    
+    func formatButtons() {
+        for button in musicPlayerButtons {
+            button.layer.cornerRadius = button.frame.size.height / 2
+        }
     }
 }
