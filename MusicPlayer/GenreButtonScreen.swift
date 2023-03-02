488d05a118bef3e410e62a6c20f4e16269e31b9e
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-03-02 10:57:17 -0300

    func genreButtonTapped set

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -18,16 +18,15 @@ class GenreButtonScreen: UIViewController {
     }
 
     @IBAction func genreButtonTapped(_ sender: UIButton) {
-        
+        playGenre(genre: sender.currentTitle!)
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        
+        musicPlayer.stop()
     }
     
-    
     @IBAction func nextButtonTapped(_ sender: UIButton) {
-        
+        musicPlayer.skipToNextItem()
     }
     
     func playGenre(genre: String) {
