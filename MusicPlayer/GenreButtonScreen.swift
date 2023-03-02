4e3345346c396b60e5d3993d99b9ca7c8cfeafcb
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-03-02 10:40:47 -0300

    genre buttons conected to viewController

--- MusicPlayer/GenreButtonScreen.swift
+++ MusicPlayer/GenreButtonScreen.swift
@@ -12,18 +12,10 @@ class GenreButtonScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
